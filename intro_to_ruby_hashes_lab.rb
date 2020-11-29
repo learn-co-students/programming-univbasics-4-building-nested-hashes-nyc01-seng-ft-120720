162dea8f7cfffab4ff03ba83c99ffed0bdb47f2c
darien bryant <darienbryant11@gmail.com>
2020-11-29 04:57:53 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,8 +6,8 @@ end
 
 def monopoly_with_second_tier
  {:railroads  =>
-      {:pieces => "4",
-      :rent_in_dollars => "",
+      {:pieces => 4,
+      :rent_in_dollars => {},
       :names => ""
       }
  }
@@ -19,21 +19,26 @@ end
 
 
 def monopoly_with_third_tier
-{:railroads => {:rent_in_dollars =>
-{:one_piece_owned => "25"},
-{:two_pieces_owned => "50"},
-{:three_pieces_owned => "100"},
-{:four_pieces_owned => "200"}
-}
-},
+{:railroads => {
+  :pieces => 4,
+  :rent_in_dollars =>
+{
+  one_piece_owned: 25,
+  two_pieces_owned: 50,
+  three_pieces_owned: 100,
+  four_pieces_owned: 200},
 
-{:railroads => {:names =>
-{:reading_railroad => {}},
-{:pennsylvania_railroad => {}},
-{:b_and_o_railroad => {}},
-{:shortline_railroad =>{}}
+  :names =>{
+  :reading_railroad => {},
+  :pennsylvania_railroad => {},
+  :b_and_o_railroad => {},
+  :shortline_railroad => {}
 }
 }
+}
+
+
+
 
 end
 
@@ -43,13 +48,24 @@ end
 
 
 def monopoly_with_fourth_tier
- {:names =>{ :reading_railroad => 
-    {:mortgage_value => "100" },
-    {:pennsylvania_railroad => {:mortgage_value => "400"} },
-     {:b_and_o_railroad => {:mortgage_value => "800" }},
-      {:shortline_railroad => {:mortgage_value => "800" } }
- }
- }
+{:railroads => {
+  :pieces => 4,
+  :rent_in_dollars =>
+{
+  one_piece_owned: 25,
+  two_pieces_owned: 50,
+  three_pieces_owned: 100,
+  four_pieces_owned: 200},
+
+  :names =>{
+  :reading_railroad => {:mortgage_value => 100},
+  :pennsylvania_railroad => {:mortgage_value => 200},
+  :b_and_o_railroad => {:mortgage_value => 400},
+  :shortline_railroad => {:mortgage_value => 800}
+}
+}
+}
+
  
 
 end
